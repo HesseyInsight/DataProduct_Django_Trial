f9b7c937d00543d4cae13accc5af38a057301d35
HesseyInsight <leeyoungjeon0511@gmail.com>
2019-11-07 14:38:21 +0900

    Django app, Reviewing

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
-
+def img_search(request, img_id):
+    google_url = 'www.ggl.~~'
+    total_url = google_url + img_id
+    return redirect(total_url)
+    pass
 # models.py파일에 정의된 모델을 가져올 것이다. 
 # 다음 step에서 QuerySet을 Template에 보내는 방법을 배워보도록 한다.
 
